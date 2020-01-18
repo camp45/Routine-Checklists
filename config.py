84750d1845f0c6f7738200d63eba026f4c14aeff
Brad <campbelb6262@gmail.com>
2020-01-18 17:42:53 -0600

    fixed typo

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, .env))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-not-secret'
 
-    DEBUG = os.environ.get('DEBUG') or FALSE
+    DEBUG = os.environ.get('DEBUG') or False
 
     #SQL Config
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
