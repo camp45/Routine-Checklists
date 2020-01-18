542acab0c974b45999862eb069b30d4968ee4f08
Brad <campbelb6262@gmail.com>
2020-01-18 17:39:35 -0600

    Updated config to use python-dotenv package and .evn files

--- config.py
+++ config.py
@@ -1,9 +1,18 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, .env))
 
 class Config(object):
+
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-not-secret'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+
+    DEBUG = os.environ.get('DEBUG') or FALSE
+
+    #SQL Config
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_RECYCLE = os.environ.get('SQLALCHEMY_POOL_RECYCLE') or 299
+
