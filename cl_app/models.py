be54451f3e6331bca1f37cb681a41937503f6da6
Brad <campbelb6262@gmail.com>
2020-05-20 18:50:09 -0500

    Added generate_password and check_password methods to User model

--- cl_app/models.py
+++ cl_app/models.py
@@ -1,5 +1,5 @@
 from cl_app import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,3 +9,9 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+    def generate_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
