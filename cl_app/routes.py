30ef6ef66b69427b96a3fb6f39e5ffd12de1401a
Brad <campbelb6262@gmail.com>
2020-05-20 21:33:55 -0500

    Finished login route, template, form and other infrastructure for login and logout.

--- cl_app/routes.py
+++ cl_app/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user
 
 from cl_app import app
 from cl_app.forms import LoginForm
+from cl_app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -26,8 +28,21 @@ def create_new_checklist():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f'User {form.username.data} logged in!')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password!')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        flash(f'Welcome {user.username}.')
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
