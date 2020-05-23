7f9f7c5c40fb6c1b367edddda4cbed516f06ff35
Brad <campbelb6262@gmail.com>
2020-05-23 11:06:16 -0500

    Added the CheckList and ListItem tables to the app. Associated files are included, migrations and such.

--- cl_app/models.py
+++ cl_app/models.py
@@ -1,6 +1,7 @@
 from cl_app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class User(UserMixin, db.Model):
@@ -8,6 +9,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    checklists = db.relationship('CheckList', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -21,3 +23,27 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+
+class CheckList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    creation_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    listitems = db.relationship('ListItem', backref='checklist', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<CheckList: {self.title}>'
+
+
+class ListItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, index=True)
+    checklist_id = db.Column(db.Integer, db.ForeignKey('check_list.id'))
+    creation_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<ListItem: {self.title}>'
+
+    def __repr__(self):
+        return f'<ListItem: {self.title}>'
--- routine_checklists.py
+++ routine_checklists.py
@@ -1,6 +1,10 @@
 from cl_app import app, db
-from cl_app.models import User
+from cl_app.models import User, CheckList, ListItem
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    return {'db': db,
+     'User': User,
+     'CheckList': CheckList,
+     'ListItem': ListItem
+     }
