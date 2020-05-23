ba4b9b1086dcf741bd45589afe1dae8e798cf3e2
Brad <campbelb6262@gmail.com>
2020-05-22 21:43:07 -0500

    Added Flask-debugtoolbar to the app

--- cl_app/__init__.py
+++ cl_app/__init__.py
@@ -3,13 +3,17 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.debug = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from cl_app import routes, models
+toolbar = DebugToolbarExtension(app)
+
+from cl_app import routes, models, errors
 
