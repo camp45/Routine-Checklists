7966c2e6de2dba050204a4d4deabc91790c059ea
Brad <campbelb6262@gmail.com>
2020-05-28 17:41:50 -0500

    Added flask-bootstrap. Added basic bootstrap styling to the app in the app base.html template.

--- cl_app/__init__.py
+++ cl_app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_debugtoolbar import DebugToolbarExtension
-
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -13,6 +13,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 toolbar = DebugToolbarExtension(app)
+
+bootstrap = Bootstrap(app)
 
 from cl_app import routes, models, errors
 
--- cl_app/routes.py
+++ cl_app/routes.py
@@ -45,7 +45,7 @@ def login():
             flash('Invalid username or password!')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        flash(f'Welcome {user.username}.')
+        flash(f'Welcome {user.username}')
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
